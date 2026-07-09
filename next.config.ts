00b524fa3b73f78ab695b4c9d0ab8ee3de2a8646
RobertoSantos98 <roberto.felipe467@gmail.com>
2026-07-09 13:31:12 -0300

    Changes to be committed: 	modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
+
+
+/** @type {import('next').NextConfig} */
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+
+  basePath: '/youcareclinic',
+
+  trailingSlash: true,
+
   images: {
+    unoptimized: true,
+    
     remotePatterns: [
       {
         protocol: 'https',
